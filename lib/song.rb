83ad4acc36753b780fc7a687caeefdce22d7bea8
Tristan Brown <tgbrown@stanford.edu>
2018-09-15 21:41:27 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,10 +34,7 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
-    if self.find_by_name
-      return name
-    elsif !self.find_by_name
-      self.create_by_name
+    self.find_by_name || self.create_by_name
     end
   end 
 end
