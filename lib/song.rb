d5dbacbedcb97088f699536db4da6253374f65f5
Tristan Brown <tgbrown@stanford.edu>
2018-09-15 21:37:41 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,8 +30,8 @@ class Song
   end
 
   def self.find_by_name(name)
-    self.all.find{|song| song.name = name}
-    song 
+    self.all.find{|song| song.name == name}
+  
   end
 
   def self.find_or_create_by_name(name)
